2226106213dbe1779b524bddb9c97ef93dcd0eaf
tyorytyory <hashimoto27ban@yahoo.co.jp>
2017-07-17 21:32:22 +0900

    20170717 21:32

--- Kolmo_cat.java
+++ Kolmo_cat.java
@@ -11,8 +11,8 @@ public class Kolmo_cat{
     public static void main(String[] args) throws IOException{
 
     	String Index;
-        String Kolmo[][] = new String [25][300];
-        int Kolmo_count[] = new int [25];
+        String Kolmo[][] = new String [30][300];
+        int Kolmo_count[] = new int [30];
         int number = 1;
         String Kolmo_natural = "0.0";
         String[] filename = new String [20];
@@ -83,18 +83,19 @@ public class Kolmo_cat{
 
 
 
+
         }
 
         number = 1;
 
-        if(Integer.parseInt(filename[1]) % 24 == 0){
+        if(Integer.parseInt(filename[1]) % 24 == 0 && Integer.parseInt(filename[1]) != 0){
 
-			File file = new File(filename[0] + "_" + filename[2] + "_" + filename[3]// + "_" + filename[4]  +".txt"
+			File file = new File(filename[0] + "_" + filename[2] + "_" + filename[3] + "_" + filename[4]  +".txt"
 					);
 	     	PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(file)));
 
 	     	for(int i = 1;i<=271;i++){
-	     		for(int j = 1;j<=24;j++){
+	     		for(int j = 0;j<=24;j++){//j = 0 or 1
 	         		if(Kolmo[j][i] != null && Kolmo[j][i].length() <= 30 ){
 	         			pw.print(Kolmo[j][i] + ",");
 	         			Kolmo[j][i] = null;
@@ -112,7 +113,7 @@ public class Kolmo_cat{
 	         		}
 	     		}
 	     		pw.println();
-	     		if(nodata_number == 24){
+	     		if(nodata_number == 25){//24 or 25
 	     			nodata_number = 0;
 	     			break;
 	     		}
