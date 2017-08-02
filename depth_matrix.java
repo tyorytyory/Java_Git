7ce1d5b54bee67312aa59f102c9e034d954f9365
naoki hashimoto <hashimoto27ban@yahoo.co.jp>
2017-08-02 21:09:32 +0900

    20170802 21:09

--- depth_matrix.java
+++ depth_matrix.java
@@ -43,13 +43,13 @@ public class depth_matrix{
 
          	String  JNIc_split[] = null;
 
-         	int before_move_condition = 0;
+         	int before_move_condition = 0;//直前の板の移動の情報を書き込む
          	int now_move_condition = 0;
-         	int count_up_up = 0;
-         	int count_up_down = 0;
-         	int count_down_down = 0;
-         	int count_down_up = 0;
-         	boolean morning_or_afternoon = false;
+         	int count_up_up = 0;//板が上昇し、その後も上昇する回数
+         	int count_up_down = 0;//板が上昇し、その後は下落する回数
+         	int count_down_down = 0;//板が下落し、その後も下落する回数
+         	int count_down_up = 0;//板が下落し、その後は上昇する回数
+         	boolean morning_or_afternoon = false;//前場(false) or 後場(true)
 
 
             while ((line = brtxt.readLine()) != null) {
@@ -67,32 +67,33 @@ public class depth_matrix{
             		day = JNIc_split[0];
             	}
             	else if(!(day.equals(JNIc_split[0])) || (45000 <= time_total && Integer.parseInt(day) < 20110214 && morning_or_afternoon == false)){
-            		if(morning_or_afternoon == false && Integer.parseInt(day) < 20110214 && day.equals(JNIc_split[0])){
+            		if(morning_or_afternoon == false && Integer.parseInt(day) < 20110214 && day.equals(JNIc_split[0])){//半日オークションの場合
             			pw.println(day + ",morning," + count_up_up + "," + count_up_down + "," + count_down_up + "," + count_down_down + ",,,,,,,");
             			morning_or_afternoon = true;
             		}
-            		else if(morning_or_afternoon == false && Integer.parseInt(day) < 20110214 && !(day.equals(JNIc_split[0]))){
+            		else if(morning_or_afternoon == false && Integer.parseInt(day) < 20110214 && !(day.equals(JNIc_split[0]))){//前場の書き込み
             			pw.println(day + ",morning," + count_up_up + "," + count_up_down + "," + count_down_up + "," + count_down_down + ",,,,,,,");
             			morning_or_afternoon = false;
             		}
-            		else if(morning_or_afternoon == true && Integer.parseInt(day) < 20110214){
+            		else if(morning_or_afternoon == true && Integer.parseInt(day) < 20110214){//後場の書き込み
             			pw.println(day + ",afternoon," + count_up_up + "," + count_up_down + "," + count_down_up + "," + count_down_down + ",,,,,,,");
             			morning_or_afternoon = false;
             			System.out.println(line);
             		}
-            		else if(20110214 <= Integer.parseInt(day)){
+            		else if(20110214 <= Integer.parseInt(day)){//昼休みが廃止されたとき
             			pw.println(day + ",no noon recess," + count_up_up + "," + count_up_down + "," + count_down_up + "," + count_down_down + ",,,,,,,");
             		}
+            		//------------初期化--------------
             		day = JNIc_split[0];
             		before_move_condition = 0;
             		count_up_up = 0;
             		count_up_down = 0;
             		count_down_up = 0;
             		count_down_down = 0;
-
+            		//------------初期化--------------
             	}
 
-            	if(before_move_condition == 0){
+            	if(before_move_condition == 0){//一番最初とき
             		if(JNIc_split[2].equals("up") || JNIc_split[2].equals("up not Trade")){//買板・売板の上昇
             			before_move_condition = 1;
 
@@ -101,19 +102,19 @@ public class depth_matrix{
                 		now_move_condition = 2;
                 	}
             	}
-            	else if(before_move_condition == 1 && (JNIc_split[2].equals("up") || JNIc_split[2].equals("up not Trade"))){
+            	else if(before_move_condition == 1 && (JNIc_split[2].equals("up") || JNIc_split[2].equals("up not Trade"))){//板が上昇した後の、板の上昇
             		count_up_up++;
             		before_move_condition = 1;
             	}
-            	else if(before_move_condition == 1 && (JNIc_split[2].equals("down") || JNIc_split[2].equals("down not Trade"))){
+            	else if(before_move_condition == 1 && (JNIc_split[2].equals("down") || JNIc_split[2].equals("down not Trade"))){//板が上昇した後の、板の下落
             		count_up_down++;
             		before_move_condition = 2;
             	}
-            	else if(before_move_condition == 2 && (JNIc_split[2].equals("up") || JNIc_split[2].equals("up not Trade"))){
+            	else if(before_move_condition == 2 && (JNIc_split[2].equals("up") || JNIc_split[2].equals("up not Trade"))){//板が下落した後の、板の上昇
             		count_down_up++;
             		before_move_condition = 1;
             	}
-            	else if(before_move_condition == 2 && (JNIc_split[2].equals("down") || JNIc_split[2].equals("down not Trade"))){
+            	else if(before_move_condition == 2 && (JNIc_split[2].equals("down") || JNIc_split[2].equals("down not Trade"))){//板が下落した後の、板の下落
             		count_down_down++;
             		before_move_condition = 2;
             	}
@@ -121,15 +122,15 @@ public class depth_matrix{
 
             }
 
-            if(morning_or_afternoon == false && Integer.parseInt(day) < 20110214){
+            if(morning_or_afternoon == false && Integer.parseInt(day) < 20110214){//前場
     			pw.println(day + ",morning," + count_up_up + "," + count_up_down + "," + count_down_up + "," + count_down_down + ",,,,,,,");
     			morning_or_afternoon = true;
     		}
-    		else if(morning_or_afternoon == true && Integer.parseInt(day) < 20110214){
+    		else if(morning_or_afternoon == true && Integer.parseInt(day) < 20110214){//後場
     			pw.println(day + ",afternoon," + count_up_up + "," + count_up_down + "," + count_down_up + "," + count_down_down + ",,,,,,,");
     			morning_or_afternoon = false;
     		}
-    		else if(20110214 <= Integer.parseInt(day)){
+    		else if(20110214 <= Integer.parseInt(day)){//昼休みの廃止
     			pw.println(day + ",no noon recess," + count_up_up + "," + count_up_down + "," + count_down_up + "," + count_down_down + ",,,,,,,");
     		}
 
